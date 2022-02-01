33671e045c627e5b1edcca18d98d5c33b11c2ede
EdwardOlszewski <edolszewski96@gmail.com>
2022-02-01 10:25:44 -0600

    stripe update

--- frontend/src/types/paymentTypes.js
+++ frontend/src/types/paymentTypes.js
@@ -1,5 +1,5 @@
 import { loadStripe } from '@stripe/stripe-js'
 
 export const stripePromise = loadStripe(
-  'pk_test_51KN8YZGmnGBraIyuUWpw2JQzLc7n2HwaU05uhuvdfCERUnfaTzrqPXMUR7U9IwryMYOX9EjVB0IQz38CQAP1WrlT00mwO6ZH9H'
+  'pk_live_51KN8YZGmnGBraIyujruwkMC3peKaXKKf3CZJq6a5w1YutxnjEPyYMkMsLfN81tULw8TzvUJUA96jMOVoMDDlubyC00ydJsiqjd'
 )
